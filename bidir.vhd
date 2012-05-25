ab9408ee66c34079ea752df5cb8741612b54c136
Nils Christopher Brause <nilschrbrause@googlemail.com>
2012-05-25 12:07:59 +0200

    [generics] Added bidirectional buffer.

---
+++ bidir.vhd
@@ -0,0 +1,41 @@
+-- Copyright (c) 2012, Nils Christopher Brause
+-- All rights reserved.
+-- 
+-- Permission to use, copy, modify, and/or distribute this software for any
+-- purpose with or without fee is hereby granted, provided that the above
+-- copyright notice and this permission notice appear in all copies.
+-- 
+-- THE SOFTWARE IS PROVIDED "AS IS" AND THE AUTHOR DISCLAIMS ALL WARRANTIES
+-- WITH REGARD TO THIS SOFTWARE INCLUDING ALL IMPLIED WARRANTIES OF
+-- MERCHANTABILITY AND FITNESS. IN NO EVENT SHALL THE AUTHOR BE LIABLE FOR
+-- ANY SPECIAL, DIRECT, INDIRECT, OR CONSEQUENTIAL DAMAGES OR ANY DAMAGES
+-- WHATSOEVER RESULTING FROM LOSS OF USE, DATA OR PROFITS, WHETHER IN AN
+-- ACTION OF CONTRACT, NEGLIGENCE OR OTHER TORTIOUS ACTION, ARISING OUT OF
+-- OR IN CONNECTION WITH THE USE OR PERFORMANCE OF THIS SOFTWARE.
+-- 
+-- The views and conclusions contained in the software and documentation are
+-- those of the authors and should not be interpreted as representing official
+-- policies, either expressed or implied, of the Max Planck Institute for
+-- Gravitational Physics (Albert Einstein Institute).
+
+library ieee;
+use ieee.std_logic_1164.all;
+
+entity bidir is
+  generic (
+    bits : natural);
+  port (
+    pins   : inout std_logic_vector(bits-1 downto 0);
+    output : in    std_logic_vector(bits-1 downto 0);
+    input  : out   std_logic_vector(bits-1 downto 0);
+    dir    : in    std_logic);          -- 1 = in, 0 = out
+end entity bidir;
+
+architecture behav of bidir is
+
+begin  -- architecture behav
+
+  pins <= output when dir = '0' else (others => 'Z');
+  input <= pins when dir = '1' else output;
+
+end architecture behav;
