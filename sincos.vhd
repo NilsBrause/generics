badc7901d6280cb120d9c7a0e073c56d7602096f
Nils Christopher Brause <nilschrbrause@googlemail.com>
2012-07-18 11:35:29 +0200

    [generics] sincos: make lut length selectable.

--- sincos.vhd
+++ sincos.vhd
@@ -26,7 +26,8 @@ use ieee.math_real.all;
 entity sincos is
   generic (
     phase_bits : natural;
-    bits       : natural);
+    bits       : natural;
+    lut_type   : natural := 2);
   port (
     clk    : in  std_logic;
     reset  : in  std_logic;
@@ -37,40 +38,74 @@ end entity sincos;
 
 architecture behav of sincos is
 
-  constant lut_in_bits : natural := phase_bits-2;
+  type dyn_rom1_t is array (0 to 2**phase_bits/1-1)
+    of std_logic_vector(bits-1 downto 0);
+  type dyn_rom2_t is array (0 to 2**phase_bits/2-1)
+    of std_logic_vector(bits-1 downto 0);
+  type dyn_rom4_t is array (0 to 2**phase_bits/4-1)
+    of std_logic_vector(bits-1 downto 0);
 
-  type rom_t is array (0 to 2**lut_in_bits-1) of std_logic_vector(bits-1 downto 0);
-
-  function makelut return rom_t is
-    variable tmp : rom_t;
+  function makelut1 return dyn_rom1_t is
+    variable tmp : dyn_rom1_t;
     variable x : real;
   begin
-    for c in 0 to 2**lut_in_bits-1 loop
-      x := cos(real(c)/real(2**lut_in_bits-1)*MATH_PI/real(2))*real(2**(bits-1)-1);
+    for c in 0 to 2**phase_bits-1 loop
+      x := cos(real(c)/real(2**phase_bits)*MATH_PI*real(2))*real(2**(bits-1)-1);
       tmp(c) := std_logic_vector(to_signed(integer(x), bits));
     end loop;  -- c
     return tmp;
-  end makelut;
+  end makelut1;
 
-  constant rom : rom_t := makelut;
+  function makelut2 return dyn_rom2_t is
+    variable tmp : dyn_rom2_t;
+    variable x : real;
+  begin
+    for c in 0 to 2**phase_bits/2-1 loop
+      x := cos(real(c)/real(2**phase_bits)*MATH_PI*real(2))*real(2**(bits-1)-1);
+      tmp(c) := std_logic_vector(to_signed(integer(x), bits));
+    end loop;  -- c
+    return tmp;
+  end makelut2;
+
+  function makelut4 return dyn_rom4_t is
+    variable tmp : dyn_rom4_t;
+    variable x : real;
+  begin
+    for c in 0 to 2**phase_bits/4-1 loop
+      x := cos(real(c)/real(2**phase_bits)*MATH_PI*real(2))*real(2**(bits-1)-1);
+      tmp(c) := std_logic_vector(to_signed(integer(x), bits));
+    end loop;  -- c
+    return tmp;
+  end makelut4;
+
+  constant dyn_rom1 : dyn_rom1_t := makelut1;
+  constant dyn_rom2 : dyn_rom2_t := makelut2;
+  constant dyn_rom4 : dyn_rom4_t := makelut4;
 
   signal quadrant  : std_logic_vector(1 downto 0) := (others => '0');
-  signal quadrant2 : std_logic_vector(1 downto 0) := (others => '0');
-  signal idx       : std_logic_vector(lut_in_bits-1 downto 0) := (others => '0');
   signal val0      : std_logic_vector(bits-1 downto 0) := (others => '0');
   signal val1      : std_logic_vector(bits-1 downto 0) := (others => '0');
+  signal val2      : std_logic_vector(bits-1 downto 0) := (others => '0');
+  signal val3      : std_logic_vector(bits-1 downto 0) := (others => '0');
+  signal val4      : std_logic_vector(bits-1 downto 0) := (others => '0');
+  signal val5      : std_logic_vector(bits-1 downto 0) := (others => '0');
 
 begin  -- architecture behav
-
-  quadrant2 <= phase(phase_bits-1 downto phase_bits-2);
-  idx <= phase(lut_in_bits-1 downto 0);
   
   -- make synthesizable RAM
   lutram: process (clk, reset) is
   begin
     if rising_edge(clk) then
-      val0 <= rom(to_integer(unsigned(idx)));
-      val1 <= rom(to_integer(unsigned(not idx)));
+      val0 <= dyn_rom4(to_integer(unsigned(phase(phase_bits-3 downto 0))));
+      val1 <= dyn_rom4(to_integer(unsigned(not phase(phase_bits-3 downto 0))));
+      
+      val2 <= dyn_rom2(to_integer(unsigned(phase(phase_bits-2 downto 0))));
+      val3 <= dyn_rom2(to_integer(unsigned(not phase(phase_bits-2)
+                                           & phase(phase_bits-3 downto 0))));
+      
+      val4 <= dyn_rom1(to_integer(unsigned(phase(phase_bits-1 downto 0))));
+      val5 <= dyn_rom1(to_integer(unsigned(phase(phase_bits-1 downto 0))
+                                  -2**phase_bits/4));
     end if;
   end process lutram;
   
@@ -81,18 +116,29 @@ begin  -- architecture behav
       clk      => clk,
       reset    => reset,
       enable   => '1',
-      data_in  => quadrant2,
+      data_in  => phase(phase_bits-1 downto phase_bits-2),
       data_out => quadrant);
   
-  cosout <= val0 when quadrant = "00" else
-            not val1 when quadrant = "01" else
-            not val0 when quadrant = "10" else
-            val1 when quadrant = "11" else
+  cosout <= val4 when lut_type = 1 else
+            val2 when quadrant = "00" and lut_type = 2 else
+            val2 when quadrant = "01" and lut_type = 2 else
+            not val2 when quadrant = "10" and lut_type = 2 else
+            not val2 when quadrant = "11" and lut_type = 2 else
+            val0 when quadrant = "00" and lut_type = 4 else
+            not val1 when quadrant = "01" and lut_type = 4 else
+            not val0 when quadrant = "10" and lut_type = 4 else
+            val1 when quadrant = "11" and lut_type = 4 else
             (others => '0');
-  sinout <= val1 when quadrant = "00" else
-            val0 when quadrant = "01" else
-            not val1 when quadrant = "10" else
-            not val0 when quadrant = "11" else
+  
+  sinout <= val5 when lut_type = 1 else
+            val3 when quadrant = "10" and lut_type = 2 else
+            val3 when quadrant = "01" and lut_type = 2 else
+            not val3 when quadrant = "00" and lut_type = 2 else
+            not val3 when quadrant = "11" and lut_type = 2 else
+            val1 when quadrant = "00" and lut_type = 4 else
+            val0 when quadrant = "01" and lut_type = 4 else
+            not val1 when quadrant = "10" and lut_type = 4 else
+            not val0 when quadrant = "11" and lut_type = 4 else
             (others => '0');
 
 end architecture behav;
